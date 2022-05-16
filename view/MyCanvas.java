585aa56089e27aeb43d2837b35826bae6721c72b
Anna <am.spencer@live.co.uk>
2022-05-16 23:21:09 +0100

    initialise gameboard and buttons

---
+++ view/MyCanvas.java
@@ -0,0 +1,27 @@
+package view;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.Dimension;
+
+
+public class MyCanvas extends JPanel {
+
+    private GameBoard gameBoard;
+
+    public MyCanvas(GameBoard gameBoard, int width, int height) {
+        this.gameBoard = gameBoard;
+        setPreferredSize(new Dimension(width, height));
+        setBackground(Color.BLACK);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics g2 = (Graphics2D) g;
+    }
+    
+}
