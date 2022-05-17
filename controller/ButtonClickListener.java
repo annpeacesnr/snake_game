eb32356f8c8f6ca6d51f5c74e9a6a1c59827c358
Anna <am.spencer@live.co.uk>
2022-05-17 07:06:55 +0100

    score refresh

--- controller/ButtonClickListener.java
+++ controller/ButtonClickListener.java
@@ -26,6 +26,7 @@ public class ButtonClickListener implements ActionListener {
             gameBoard.getCanvas().getFigures().add(gameBoard.getSnake());
             gameBoard.createFood(); 
             gameBoard.setScore(0);
+            gameBoard.getScoreDisplay().setText("0");
             gameBoard.setGameOver(false);
         } else if (button == gameBoard.getStopButton()) {
             String label = button.getText();
